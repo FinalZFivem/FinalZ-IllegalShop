46f1e59d03a889ece99698e82c8f302ab4a56847
Sikosz <88151230+Sikosz@users.noreply.github.com>
2024-08-03 14:53:50 +0200

    Update cl_main.lua
    UI, Standalone update

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -4,80 +4,75 @@ local blips = {}
 local Elements = {}
 local Sellements = {}
 
-function OpenSellMenu(id)
-	for i=1, #Config.Shops[id].items_to_sell, 1 do
-		Item = Config.Shops[id].items_to_sell[i]
-		Sellements[#Sellements+1] = {
-			label = Item.label,
-			name = Item.name,
+function SetNuiState(state)
+    SetNuiFocus(state, state)
+
+    if state then
+        TriggerScreenblurFadeIn(0)
+      else
+        TriggerScreenblurFadeOut(0)
+      end
+
+    SendNUIMessage({
+        type = "show",
+        enable = state
+    })
+end
+
+AddEventHandler("onResourceStop", function(resname)
+    if (resname ~= GetCurrentResourceName()) then return end
+
+    SetNuiState(false)
+    TriggerScreenblurFadeOut(0)
+  end)
+
+function OpenMenu(id, menuType)
+    local items_for_buy = {}
+    local items_to_sell = {}
+
+    for i=1, #Config.Shops[id].items_for_buy, 1 do
+        local Item = Config.Shops[id].items_for_buy[i]
+        table.insert(items_for_buy, {
+            label = Item.label,
+            name = Item.name,
             value = 1,
-            type = 'slider',
-            min = 1,
-            max = 10,
-			price = Item.price
-		}
-	end
-
-      ESX.UI.Menu.Open("default", GetCurrentResourceName(), "sell", {
-        title = Config.Locales[Config.Locale].SellMenuTitle,
-        align    = Config.menuAlign, 
-        elements = Sellements 
-      }, function(data,menu) -- OnSelect Function
-
-    
-        if data.current.name then
-            TriggerServerEvent("ItemManagement",
-            data.current.name, 
-            data.current.price, 
-            data.current.label,
-            data.current.value,
-            "sell"
-            )
-        end
-      end, function(data, menu) -- Cancel Function
-        menu.close() -- close menuű
-
-        local count = #Sellements
-        for i=0, count do Sellements[i]=nil end
-      end)
+            price = Item.price
+        })
     end
 
-
-    function OpenBuyMenu(id)
-        for i=1, #Config.Shops[id].items_for_buy, 1 do
-            Item = Config.Shops[id].items_for_buy[i]
-            Elements[#Elements+1] = {
-                label = Item.label,
-                name = Item.name,
-                value = 1,
-                type = 'slider',
-                min = 1,
-                max = 10,
+    if Config.Shops[id].enableSell then
+    for i=1, #Config.Shops[id].items_to_sell, 1 do
+        local Item = Config.Shops[id].items_to_sell[i]
+        table.insert(items_to_sell, {
+            label = Item.label,
+            name = Item.name,
+            value = 1,
                 price = Item.price
-            }
+            })
         end
-          ESX.UI.Menu.Open("default", GetCurrentResourceName(), "Sellements", {
-            title = Config.Locales[Config.Locale].BuyMenuTitle,
-            align    = Config.menuAlign, 
-            elements = Elements 
-          }, function(data,menu) -- OnSelect Function
-    
-        
-            if data.current.name then
-                TriggerServerEvent("ItemManagement",
-                data.current.name, 
-                data.current.price, 
-                data.current.label,
-                data.current.value,
-                "buy"
-                )
-            end
-          end, function(data, menu) -- Cancel Function
-            menu.close() -- close menuű
-            local count = #Elements
-            for i=0, count do Elements[i]=nil end
-          end)
-        end
+    end
+
+    SendNUIMessage({
+        type = 'show',
+        items_for_buy = items_for_buy,
+        items_to_sell = items_to_sell,
+        enableSell = Config.Shops[id].enableSell,
+        enable = true,
+        menuType = menuType,
+        locale = Config.Locale
+    })
+    SetNuiState(true)
+end
+
+RegisterNUICallback('ItemManagement', function(data, cb)
+    TriggerServerEvent("ItemManagement", data.name, data.price, data.label, data.value, data.type)
+    cb('ok')
+end)
+
+RegisterNUICallback('exit', function(data, cb)
+    SetNuiState(false)
+    cb('ok')
+end)
 
 function initShops()
     for k, v in pairs(Config.Shops) do
@@ -90,121 +85,77 @@ function initShops()
         TaskSetBlockingOfNonTemporaryEvents(ped, true)
         TaskStartScenarioInPlace(ped, "WORLD_HUMAN_CLIPBOARD", -1, false)
         if v.label.enabled then
-            local label = CreateMpGamerTag(peds[k], v.label.label, false, false, "", 0)
+            local label = CreateMpGamerTag(peds[#peds], v.label.label, false, false, "", 0)
             SetMpGamerTagsVisibleDistance(v.label.distance)
             nametags[#nametags + 1] = label
         end
 
         if Config.Target then
-            exports.ox_target:addLocalEntity(peds, {
+            exports.ox_target:addLocalEntity(ped, {
                 {
                     label = Config.Locales[Config.Locale].BuyTarget,
                     name = k .. "buyMenu",
-                    icon = "fa-solid fa-fish-fins",
+                    icon = "fa-solid fa-gun",
                     distance = 2.5,
                     onSelect = function()
-                        OpenBuyMenu(k)
+                        OpenMenu(k, "buy")
                     end,
                 },
             })
-            if Config.EnableSell then
-                exports.ox_target:addLocalEntity(peds, {
-                    {
-                        label = Config.Locales[Config.Locale].SellTarget,
-                        name = k .. "SellMenu",
-                        icon = "fa-solid fa-fish-fins",
-                        distance = 2.5,
-                        onSelect = function()
-
-                            OpenSellMenu(k)
-                        end,
-                    },
-                })
-            end
         else
-            for k,v in pairs(Config.Shops) do
-                local point = lib.points.new({
-                    coords = v.coords,
-                    distance = 2,
-                })
-    
-                function point:onEnter()
-                    if Config.Textui_enabled then 
-                    Config.TextUI(Config.Locales[Config.Locale].textUI,"show")
+            local point = lib.points.new({
+                coords = v.coords,
+                distance = 2,
+            })
+
+            function point:onEnter()
+                if Config.Textui_enabled then
+                    Config.TextUI(Config.Locales[Config.Locale].textUI, "show")
                 end
             end
-                 
-                function point:onExit()
-                    Config.TextUI(Config.Locales[Config.Locale].textUI,"hide")
-                end
-                 
-                function point:nearby()
-                    local marker = lib.marker.new({
-                        type = v.MarkerSettings.type,
-                        coords = v.coords,
-                        width = v.MarkerSettings.width,
-                        height  = v.MarkerSettings.height,
-                        color = { r = v.MarkerSettings.rgba.r, g = v.MarkerSettings.rgba.g, v.MarkerSettings.rgba.b, a = v.MarkerSettings.rgba.a },
-                        rotation = v.MarkerSettings.rotate
-                    })
-                    marker:draw()
 
-                    if self.currentDistance < 2 and IsControlJustReleased(0, 38) then
-                        ESX.UI.Menu.Open("default", GetCurrentResourceName(), "choose", {
-                            title = Config.Locales[Config.Locale].TypeMenuTitle,
-                            align    = Config.menuAlign, 
-                            elements = {
-                                {label = Config.Locales[Config.Locale].SelectBuy, name = "buy"},
-                                {label = Config.Locales[Config.Locale].SelectSell, name = "sell"},
-                            } 
-                          }, function(data,menu) -- OnSelect Function
-                    
-                        
-                            if data.current.name == "buy" then
-                                OpenBuyMenu(k)
-                            elseif data.current.name == "sell" then
-                                OpenSellMenu(k)
-                            end
-                          end, function(data, menu) -- Cancel Function
-                            menu.close() -- close menuű
-                            local count = #Elements
-                            for i=0, count do Elements[i]=nil end
-                          end)
-                        end
-                    end
+            function point:onExit()
+                Config.TextUI(Config.Locales[Config.Locale].textUI, "hide")
+            end
+
+            function point:nearby()
+                local marker = lib.marker.new({
+                    type = v.MarkerSettings.type,
+                    coords = v.coords,
+                    width = v.MarkerSettings.width,
+                    height = v.MarkerSettings.height,
+                    color = { r = v.MarkerSettings.rgba.r, g = v.MarkerSettings.rgba.g, b = v.MarkerSettings.rgba.b, a = v.MarkerSettings.rgba.a },
+                    rotation = v.MarkerSettings.rotate
+                })
+                marker:draw()
+
+                if self.currentDistance < 2 and IsControlJustReleased(0, 38) then
+                    OpenMenu(k, "choose")
                 end
-            --text ui - marker - oxpoints
+            end
         end
+
         if v.blip.enabled then
             local blip = AddBlipForCoord(v.coords.xyz)
             blips[#blips + 1] = blip
-            SetBlipSprite(blips[k], v.blip.sprite)
-            SetBlipColour(blips[k], v.blip.colour)
-            SetBlipDisplay(blips[k], 4)
-            SetBlipAsShortRange(blips[k], true)
-            SetBlipScale(blips[k], v.blip.scale)
+            SetBlipSprite(blips[#blips], v.blip.sprite)
+            SetBlipColour(blips[#blips], v.blip.colour)
+            SetBlipDisplay(blips[#blips], 4)
+            SetBlipAsShortRange(blips[#blips], true)
+            SetBlipScale(blips[#blips], v.blip.scale)
             BeginTextCommandSetBlipName("STRING")
             AddTextComponentString(v.blip.label)
-            EndTextCommandSetBlipName(blips[k])
+            EndTextCommandSetBlipName(blips[#blips])
         end
     end
 end
 
 CreateThread(function()
     initShops()
-    while true do
-        Wait(500)
-        for k, v in pairs(Config.Shops) do
-            local distance = #(GetEntityCoords(cache.ped ) - v.coords.xyz)
-
-            if distance > 2.5 then
-                ESX.UI.Menu.CloseAll()
-            end
-        end
-    end
 end)
+
 function tablelength(T)
     local count = 0
     for _ in pairs(T) do count = count + 1 end
     return count
-  end
+end
